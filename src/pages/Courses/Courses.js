0fe8b0834eb74b812b7e33bfbfc35dfaa3430b9b
Muhammed Çiçekel <muhammedcicekel@gmail.com>
2023-10-18 15:45:26 +0300

    The courses page is finished.

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import './Courses.css'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
+import React, { useState } from 'react';
+import './Courses.css';
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import CourseCard from '../../Components/Cards/CourseCard/CourseCard';
+import FatherAndSon from '../../assets/images/father-and-son.jpg'
+import Nature from '../../assets/images/nature.jpg'
+import Paper from '../../assets/images/paper.jpg'
 
 export default function Courses() {
+
+  const [filter, setFilter] = useState('');
+
   return (
     <div>
       <Header/>
+      <main className='main-courses'>
+        <div className='top'>
+          <h2>All Courses</h2>
+          <div>
+            <span
+              className={filter === 'All' ? 'selected-filter' : ''}
+              onClick={() => setFilter('All')}
+            >
+              All
+            </span>
+            <span
+              className={filter === 'Design' ? 'selected-filter' : ''}
+              onClick={() => setFilter('Design')}
+            >
+              Design
+            </span>
+          </div>
+        </div>
+        <section className='courses'>
+          <CourseCard image={FatherAndSon} title='Marketing Analytics 101'/>
+          <CourseCard image={Nature} title='Marketing Analytics 101'/>
+          <CourseCard image={Paper} title='Marketing Analytics 101'/>
+        </section>
+      </main>
       <Footer/>
     </div>
   )
